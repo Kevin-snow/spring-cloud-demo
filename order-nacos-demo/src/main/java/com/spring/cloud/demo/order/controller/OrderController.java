e16aeaf104242367487c5afe0ffbf284b116af74
liyachao <beijing_yaya_2011@163.com>
2024-09-05 14:50:45 +0800

    新增服务

--- order-nacos-demo/src/main/java/com/spring/cloud/demo/order/controller/OrderController.java
+++ order-nacos-demo/src/main/java/com/spring/cloud/demo/order/controller/OrderController.java
@@ -1,8 +1,6 @@
 package com.spring.cloud.demo.order.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
