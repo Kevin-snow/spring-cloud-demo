785473368116e6e35d4b74fbb2fce138179b8893
kevin <mr_liyachao@163.com>
2024-09-05 15:01:48 +0800

    新增 springboot-rabbitmq项目

---
+++ springboot-rabbit-demo/src/main/java/com/example/springbootrabbitdemo/SpringbootRabbitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrabbitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitDemoApplication.class, args);
+    }
+
+}
